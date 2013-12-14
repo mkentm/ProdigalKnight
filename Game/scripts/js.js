ecde06fa1d1e71a00f6d062fa24294cc40942477
maximlebedev2009 <maximlebedev2009@rambler.ru>
2013-12-14 20:08:02 +0200

    Improve function ProdigalKnight

--- Game/scripts/js.js
+++ Game/scripts/js.js
@@ -7,59 +7,54 @@ $(window).load(function () {
 });
 
 var ProdigalKnight = function () {
-    var self = this;
     var doc = document;
+    var animationSpeed = 0;
 
-    /*============================================================================================*/
-    /* Utility / Mathematical / Miscellaneous */
-    /*============================================================================================*/
-    self.get = function (a) {
-        return doc.getElementById(a);
-    };
+    // window content
+    var windows = doc.querySelectorAll('.window');
+    var gameWindow = get('game');
+
+    // button content
+    // - main
+    var playGameButton = get('playGameBtn');
 
     /*============================================================================================*/
     /* Initialize Game */
     /*============================================================================================*/
-    self.initGame = function () {
-        self.bindMenuEvents();
-        self.animationSpeed = 300;
+    this.initGame = function () {
+        bindMenuEvents();
+        animationSpeed = 300;
     };
+
+    /*============================================================================================*/
+    /* Utility / Mathematical / Miscellaneous */
+    /*============================================================================================*/
+    function get (a) {
+        return doc.getElementById(a);
+    }
 
     /*============================================================================================*/
     /* Menu Interaction / Event Binding */
     /*============================================================================================*/
-    // window content
-    self.gameWrap = self.get('game-wrap');
-    self.menuBar = self.get('menu-bar');
-    self.windows = doc.querySelectorAll('.window');
-    self.mainWindow = self.get('main');
-    self.gameWindow = self.get('game');
-
-    // button content
-    // - main
-    self.playGameButton = self.get('playGameBtn');
-    self.overallStatsButton = self.get('overallStatsBtn');
-    self.resetGameDataButton = self.get('resetGameDataBtn');
-
     // hide all windows
-    self.hideWindows = function () {
-        $(self.windows).fadeTo(self.animationSpeed, 0, function () {
+    function hideWindows () {
+        $(windows).fadeTo(animationSpeed, 0, function () {
             $(this).hide();
         });
-    };
+    }
 
     // bind button events
-    self.bindMenuEvents = function () {
-        self.playGameButton.addEventListener('click', self.playGame, false);
-    };
+    function bindMenuEvents () {
+        playGameButton.addEventListener('click', playGame, false);
+    }
 
-    self.playGame = function (e) {
+    function playGame (e) {
         e.preventDefault();
-        self.hideWindows();
-        $(self.gameWindow).stop().fadeTo(self.animationSpeed, 1);
-        var e = document.createElement("script");
-        e.src = "scripts/game.js";
-        e.type="text/javascript";
-        document.getElementsByTagName("head")[0].appendChild(e);
-    };
+        hideWindows();
+        $(gameWindow).stop().fadeTo(animationSpeed, 1);
+        var script = document.createElement("script");
+        script.src = "scripts/game.js";
+        script.type="text/javascript";
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
 }; // end game
