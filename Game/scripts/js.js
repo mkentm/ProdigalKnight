9cb169570a4b2c449fc76709d7fbe2dd022fd402
maximlebedev2009 <maximlebedev2009@rambler.ru>
2013-12-14 20:48:23 +0200

    Add menu lists levels

--- Game/scripts/js.js
+++ Game/scripts/js.js
@@ -17,16 +17,22 @@ var ProdigalKnight = function () {
     // window content
     var mainWindow = get('main');
     var windows = doc.querySelectorAll('.window');
+    var levelsWindow = get('levels');
     var overallStatsWindow = get('overall-stats');
     var gameWindow = get('game');
     // button content
     // - main
     var playGameButton = get('playGameBtn');
     var overallStatsButton = get('overallStatsBtn');
+    // - main - levels
+    var levelButtons = doc.querySelectorAll('.level');
+    var returnFromLevelsToMainButton = get('returnFromLevelsToMainBtn');
+    // - main - levels - misc
+    var completedLevelsDisplay = get('completedLevels');
     // - main - overallStats
     var returnFromStatsToMainButton = get('returnFromStatsToMainBtn');
     // DOM Object
-    var dom ={};
+    var dom = {};
     dom.menuAudio = $('#audio-select');
     dom.menuBar = $('#menu-bar');
     dom.buttonAudioTrue = $('#audioTrue');
@@ -36,6 +42,8 @@ var ProdigalKnight = function () {
     /* Initialize Game */
     /*============================================================================================*/
     this.initGame = function () {
+        // set completed levels display
+        completedLevelsDisplay.innerHTML = 0;
         bindMenuEvents();
         animationSpeed = 300;
     };
@@ -43,7 +51,7 @@ var ProdigalKnight = function () {
     /*============================================================================================*/
     /* Utility / Mathematical / Miscellaneous */
     /*============================================================================================*/
-    function get (a) {
+    function get(a) {
         return doc.getElementById(a);
     }
 
@@ -51,15 +59,16 @@ var ProdigalKnight = function () {
     /* Menu Interaction / Event Binding */
     /*============================================================================================*/
     // hide all windows
-    function hideWindows () {
+    function hideWindows() {
         $(windows).fadeTo(animationSpeed, 0, function () {
             $(this).hide();
         });
     }
 
     // bind button events
-    function bindMenuEvents () {
+    function bindMenuEvents() {
         playGameButton.addEventListener('click', playGame, false);
+        returnFromLevelsToMainButton.addEventListener('click', returnToMain, false);
         overallStatsButton.addEventListener('click', overallStats, false);
         returnFromStatsToMainButton.addEventListener('click', returnToMain, false);
 
@@ -78,27 +87,40 @@ var ProdigalKnight = function () {
             dom.menuBar.stop().fadeTo(animationSpeed, 1);
             $(mainWindow).stop().fadeTo(animationSpeed, 1);
         });
+
+        // bind all levels
+        var i = levelButtons.length;
+        while (i--) {
+            levelButtons[i].addEventListener('click', startLevel, false);
+        }
     }
 
-    function playGame (e) {
+    function playGame(e) {
         e.preventDefault();
         hideWindows();
-        $(gameWindow).stop().fadeTo(animationSpeed, 1);
-        var script = document.createElement("script");
-        script.src = "scripts/game.js";
-        script.type="text/javascript";
-        document.getElementsByTagName("head")[0].appendChild(script);
+        $(levelsWindow).stop().fadeTo(animationSpeed, 1);
+
     }
 
-    function overallStats (e) {
+    function overallStats(e) {
         e.preventDefault();
         hideWindows();
         $(overallStatsWindow).stop().fadeTo(animationSpeed, 1);
     }
 
-    function returnToMain (e) {
+    function returnToMain(e) {
         e.preventDefault();
         hideWindows();
         $(mainWindow).stop().fadeTo(animationSpeed, 1);
+    }
+
+    function startLevel(e) {
+        e.preventDefault();
+        hideWindows();
+        $(gameWindow).stop().fadeTo(animationSpeed, 1);
+        var script = document.createElement("script");
+        script.src = "scripts/game.js";
+        script.type = "text/javascript";
+        document.getElementsByTagName("head")[0].appendChild(script);
     }
 }; // end game
